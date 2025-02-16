31c909d3376e38c1bc7dccb7d56d8bef1c9c49c8
kavitha0507 <kavithamynala@gmail.com>
2025-02-15 18:47:34 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+// app.js
+// Example of adding interactivity later
+document.querySelector(".download-btn").addEventListener("click", function() {
+    alert("Your resume will be downloaded!");
+  });
+  
